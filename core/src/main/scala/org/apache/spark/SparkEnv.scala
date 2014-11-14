072d353e9450c8b7c432379a0c425eba160220b8
Rafal Kwasny <Rafal_Kwasny@scee.net>
2014-11-14 10:26:18 +0000

    Revert "SPARK-2711. Create a ShuffleMemoryManager to track memory for all spilling collections"
    This reverts commit 4fde28c2063f673ec7f51d514ba62a73321960a1.
    
    Conflicts:
    	core/src/main/scala/org/apache/spark/SparkEnv.scala
    	core/src/main/scala/org/apache/spark/util/collection/ExternalSorter.scala

--- core/src/main/scala/org/apache/spark/SparkEnv.scala
+++ core/src/main/scala/org/apache/spark/SparkEnv.scala
@@ -34,7 +34,7 @@ import org.apache.spark.metrics.MetricsSystem
 import org.apache.spark.network.ConnectionManager
 import org.apache.spark.scheduler.LiveListenerBus
 import org.apache.spark.serializer.Serializer
-import org.apache.spark.shuffle.{ShuffleMemoryManager, ShuffleManager}
+import org.apache.spark.shuffle.ShuffleManager
 import org.apache.spark.storage._
 import org.apache.spark.util.{AkkaUtils, Utils}
 
@@ -65,8 +65,11 @@ class SparkEnv (
     val httpFileServer: HttpFileServer,
     val sparkFilesDir: String,
     val metricsSystem: MetricsSystem,
-    val shuffleMemoryManager: ShuffleMemoryManager,
     val conf: SparkConf) extends Logging {
+
+  // A mapping of thread ID to amount of memory, in bytes, used for shuffle aggregations
+  // All accesses should be manually synchronized
+  val shuffleMemoryMap = mutable.HashMap[Long, Long]()
 
   private val pythonWorkers = mutable.HashMap[(String, Map[String, String]), PythonWorkerFactory]()
 
@@ -221,8 +224,6 @@ object SparkEnv extends Logging {
     val shuffleMgrClass = shortShuffleMgrNames.getOrElse(shuffleMgrName.toLowerCase, shuffleMgrName)
     val shuffleManager = instantiateClass[ShuffleManager](shuffleMgrClass)
 
-    val shuffleMemoryManager = new ShuffleMemoryManager(conf)
-
     val blockManagerMaster = new BlockManagerMaster(registerOrLookup(
       "BlockManagerMaster",
       new BlockManagerMasterActor(isLocal, conf, listenerBus)), conf)
@@ -263,6 +264,9 @@ object SparkEnv extends Logging {
       "."
     }
 
+    //val shuffleManager = instantiateClass[ShuffleManager](
+    //  "spark.shuffle.manager", "org.apache.spark.shuffle.hash.HashShuffleManager")
+
     // Warn about deprecated spark.cache.class property
     if (conf.contains("spark.cache.class")) {
       logWarning("The spark.cache.class property is no longer being used! Specify storage " +
@@ -284,7 +288,6 @@ object SparkEnv extends Logging {
       httpFileServer,
       sparkFilesDir,
       metricsSystem,
-      shuffleMemoryManager,
       conf)
   }
 
